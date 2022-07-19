3e515ae02e34721815c0e2fd68ac55fef7bf2a5e
Gokulnath Ramu <gokulnath.r@prodapt.com>
2022-07-19 09:34:46 +0530

    source code

---
+++ SchoolRecords.java
@@ -0,0 +1,12 @@
+package com.prodapt.basics;
+
+public class SchoolRecords {
+	int id;
+	String name,address;
+	void ddRecords(int id,String name,String address)
+	{
+	System.out.println(id+" "+name+" "+address);
+	System.out.println("This is the school records");
+	}
+
+}
